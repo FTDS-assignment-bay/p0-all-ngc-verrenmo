017bea8607ff2190d008f0871a31b72f4f92fcc5
Verren Monica <verrenmonica26@gmail.com>
2024-11-13 21:23:14 +0700

    FTDS-NGC10-Verren

--- h8-ngc-api.py
+++ h8-ngc-api.py
@@ -26,15 +26,20 @@ weatherData = [
 # First endpoint (/weather/{location}) provide weather data for a spesific location
 @app.get('/weather/{location}')
 def displayData(location: str):
+    # Looping dari list weatherData untuk mencari location yang sama, jika tidak ada yang sama maka akan raise error
     for city in weatherData:
-        if city["Location"].lower() == location.lower():
+        # Jika location yang diinput sama dengan location dalam list maka akan megeluarkan data cuaca location tersebut. 
+        # "%20" di replace ke " " karena ketika input spasi di vercel akan terbaca sebagai %20
+        if city["Location"].lower() == location.lower().replace("%20", " "):
             return city
+        
     raise HTTPException(status_code=404, detail="Location not found!")
         
 
 # Second endpoint (/authenticate) handle api key authentication
 @app.get('/authenticate')
 def authenticate(pw: str = Header(None)):
+    # Jika password yang diinput tidak sama dengan password "weather1234" maka akan raise error Unauthorized
     if pw != password:
         raise HTTPException(status_code=401,detail="Unauthorized!" )
     else:
